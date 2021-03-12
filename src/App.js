b5100826817b69989fa891e01cfcde3e5d0ad118
Saurav1999 <singhsaurav1999@gmail.com>
2021-03-12 15:59:24 +0530

    React Custom Pagination Completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import JsonData from "./MOCK_DATA.json";
+import ReactPaginate from "react-paginate";
+import "./App.scss";
 
 function App() {
+  const [users, setUsers] = useState(JsonData.slice(0, 100));
+  const [pageNumber, setPageNumber] = useState(0);
+  const itemsPerPage = 10;
+  const pagesVisited = pageNumber * itemsPerPage;
+  const pageCount = Math.ceil(users.length / itemsPerPage);
+  const changePage = ({ selected }) => {
+    setPageNumber(selected);
+  };
+  const displayUsers = users
+    .slice(pagesVisited, pagesVisited + itemsPerPage)
+    .map((user) => {
+      return (
+        <div className="user">
+          <h3>{user.first_name}</h3>
+          <h3>{user.last_name}</h3>
+          <h3>{user.email}</h3>
+        </div>
+      );
+    });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>USERS</h2>
+      <div className="users-container">{displayUsers}</div>
+
+      <ReactPaginate
+        previousLabel="Previous"
+        nextLabel="Next"
+        pageCount={pageCount}
+        onPageChange={changePage}
+        pageRangeDisplayed={4}
+        marginPagesDisplayed={0}
+        containerClassName={"paginationButtons"}
+        previousLinkClassName={"previousButton"}
+        nextLinkClassName={"nextButton"}
+        disabledClassName={"paginationDisabled"}
+        activeClassName={"paginationActive"}
+      />
     </div>
   );
 }
